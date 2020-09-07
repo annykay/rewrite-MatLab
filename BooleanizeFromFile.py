90dc7163b4d0bead751e71febf48d77c3cfbbf6e
Анна <52636876+annykay@users.noreply.github.com>
2020-09-07 20:55:49 +0300

    Update BooleanizeFromFile.py
    "workig" version with comments

--- BooleanizeFromFile.py
+++ BooleanizeFromFile.py
@@ -1,49 +1,56 @@
 
-# coding: utf-8
-
-# In[ ]:
-
-
 def BooleanizeFromFile(InputData, OutputFile ):
-    import scipy.io
-    import numpy as np 
-    mat = scipy.io.loadmat('Thresholds_CoTFcRF.mat')#возвращает словарь. под ключем dists "матрица": [0:2136][0][0][0:1][0:1000]
-    #dt_1 = np.dtype([('Ens', 'S16'), ('Triv', 'S10')], align=True)
-    #with open('background_genes.txt') as inp:
-    #    backgroundgenes, triv = np.loadtxt(inp, dtype=dt_1, unpack = True)
-    #print(type(backgroundgenes[1]))
-    dt_1 = np.dtype([('Ens', (str, 16)), ('Triv', (str, 10))], align=True)
-    with open('background_genes.txt') as inp:
-        backgroundgenes = np.loadtxt(inp, dtype=dt_1)#считываем данные в numpy массив пар значений(возможно, придется исправить)
-    print(InputData, OutputFile)
-    dt_2 = np.dtype([('Ens', (str, 20)), ('Triv', (str,10)), ('exp', int)])#то же самое, но тройки значений
-    with open(InputData) as InpD:
-        inpData = np.genfromtxt(InpD, dtype = dt_2, names = True)
+    """ function for 
+        :InputData: str - 
+        :OutputFile: str - the name of a file, where the results would be written 
+        :return: -
+        """
+    import scipy.io #need for loading .mat files 
+    import numpy as np #need for calculations 
+    mat = scipy.io.loadmat('Thresholds_CoTFcRF.mat') # load data from 'Treshholds_CoTFcRF.mat'. Returns a dict, numeric data is under 
+                                                     # the key 'dists'. mat['dists'] is a tensor with the shapes : [0:2136][0][0][0:1][0:999]
     
-    inputmat = -1*np.ones((2137,1)) #может быть исправить длину 2137??
-    #print(inpData[0])
-    for i in range(2137):# тут то же самое 
-        #print(backgroundgenes[i][0])
-        #print(inpData['ensembl_gene_id'][1])
-        idx = np.nonzero(backgroundgenes[i][0] == inpData['ensembl_gene_id'])#может быть поблемма из-за регистра
-        if idx[0]:
+    dt_1 = np.dtype([('Ens', (str, 16)), ('Triv', (str, 10))], align=True) # special datatype for loading data from 'background_genes.txt' 
+    with open('background_genes.txt') as inp: 
+        backgroundgenes = np.loadtxt(inp, dtype=dt_1)#loading data from 'background_genes.txt'  to a numpy matrix with pairs of values
+    print(InputData, OutputFile) 
+    dt_2 = np.dtype([('ensembl_gene_id', (str, 20)), ('hgnc_symbol', (str,10)), ('TPM', int)])#special datatype for loading data from InputData 
+    with open(InputData) as InpD:
+        inpData = np.genfromtxt(InpD, dtype = dt_2, names = True)#loading input data
+    
+    inputmat = -1*np.ones((2137,1)) #making an array to get rid of genes, that are not in backgroundgenes
+    #maybe, it's worth to write len(inpData) instead of 2137
+    
+    for i in range(2137):#getting rid of genes, that are not in backgroundgenes array
+        
+        idx = np.nonzero(inpData['ensembl_gene_id']==backgroundgenes['Ens'][i] )#findibg indexis of genes  in inpData, that are not in backgroundgenes
+
+        if len(idx[0])>0: # here and after: np.nonzero returns two arrays: colmn index and raw index. the index of a raw is important
             inputmat[i] = inpData['TPM'][idx[0][0]]
-    existing = np.nonzero(inputmat>-1)[0]
+
+    existing = np.nonzero(inputmat>-1)[0]#finding values in inputmat that are mire than -1 
     #print(existing)
-    dists_inp = np.array([mat['dists'][i][0][0] for i in existing])#выглядит ужасно, надо исправить 
-    backgroundgenes_inp = [backgroundgenes['Triv'][i] for i in existing]
-    #print(dists_inp)
-    exp_inp = [inputmat[i] for i in existing]
+    dists_inp = np.array([mat['dists'][i][0][0] for i in existing])#reduce amount of indexes  
+    backgroundgenes_inp = [backgroundgenes['Triv'][i] for i in existing] #reduce amount of indexes
+    #print('dists_inp -------------->', dists_inp)
+    exp_inp = [inputmat[i] for i in existing] 
     exp_inp = np.array(exp_inp)
+    #print(max(exp_inp))
+    #calculating Pvals 
     Pvals_low,Pvals_up,Pvals_inter_low,Pvals_inter_up,Pvals_low_adj,Pvals_up_adj, Pvals_inter_low_adj,Pvals_inter_up_adj = CalculatePvalues(dists_inp, exp_inp)
     s = len(exp_inp)
-    
-    
-    #тут я остановилась
-    Bool_exp = np.zeros((s,1))
+ 
+    Bool_exp = np.zeros((s,1)) #initializing an array for output 
         
-    
+    #print(Pvals_low_adj)
     for i in np.nonzero(Pvals_low_adj>0.1)[0]:
         Bool_exp[i]=1
-    output = [(backgroundgenes_inp[gene], Bool_exp[gene]) for gene in range(s)]
+    output = {backgroundgenes_inp[gene]: Bool_exp[gene] for gene in range(s)}#making a dict for output data 
+    
+    # writing data into OutputFile as a column of paris gene_name and 1 or zero
+    with open(OutputFile, 'w') as f:
+        for key,val in output.items():
+            f.write('{}, {}\n'.format(key,val[0]))
+
+   
 
