7843509759620b0665a4c6afbcd7a193114b9f3e
Анна <52636876+annykay@users.noreply.github.com>
2020-09-07 21:28:53 +0300

    Update ecdf.py

--- ecdf.py
+++ ecdf.py
@@ -1,11 +1,17 @@
 
-# coding: utf-8
-
-# In[ ]:
-
-
 def ecdf(x, freq):
-    
+    """calculete empirical destribtion function for values x with frequences freq
+       :x:list - values 
+       :freq:list - frequences of x
+       :return: x - x values for ecdf
+       :return: ys - y values for ecdf 
+    """
+    #print(len(np.nonzero(x)[0]))
+    if len(np.nonzero(x)[0])==0:# if all x are 0 
+        x = np.arange(0, len(x), 1)
+        ys = [1 for i in range(len(x))]
+        return x, ys
+    #print('first x ---------->', x)
     x, freq  = zip(*sorted(zip(x,freq)))
     #xs = np.sort(x)
     if freq[0]==0:
@@ -14,5 +20,7 @@ def ecdf(x, freq):
     else:
         ys = np.cumsum(freq)/np.sum(freq)
     #ys = np.arange(1, len(xs)+1)/float(len(xs))
+    #print('second x --------------------->', x)
+    if not isinstance(x[0], float):
+        x = [x[i][0] for i in range(len(x))]
     return x, ys
-
